ddb857a55e39d83d51759be43a4356d884b21990
Rob Lovelett <rlovelett@gmail.com>
2016-11-03 17:57:11 -0500

    second commit

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <link rel="stylesheet" href="./bootstrap.css" media="screen">
+    <link rel="stylesheet" href="../assets/css/custom.min.css">
+    <title>Basic Portfolio: Robert Lovelett</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    </head>
+    <body>
+        <header>
+        <nav class="navbar navbar-default">
+            <div class="container">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">Robert Lovelett</a>
+                </div>
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="active"><a href="#">About</a></li>
+                    <li><a href="#">Portfolio</a></li>
+                    <li><a href="#">Contact</a></li> 
+                </ul>
+            </div>
+        </nav>
+        </header>
+        <main>
+            <div class="main-wrapper">
+                <div class="container">
+                    <div class="row">
+                        <div class="main col-md-8">
+                        <article>
+                            <h1>About Me</h1>
+                            <hr />
+                            <img class="profile pull-left" src="assets/images/profile.png" alt="Robert Lovelett" />
+                            <p>My name is Robert Lovelett and I am an animator, illustrator, designer, storyteller, and soon to be full stack developer. I live in Austin, TX with my wife and two cats.</p>
+                            <p>I have worked professionally since 2005. The fields have run the gamut: from online games and independent film, to marketing campaigns, spectacle theater, and AAA videogames. I hope to retain my own voice with each client I work for, creating a humorous but meaningful experience for all people.</p>
+                            <p>I am currently studying to be a full stack developer.</p>
+                        </article>
+                        </div>
+                        <div class="sidebar col-md-3">
+                            <aside>
+                                <h2>Connect with Me</h2>
+                                <hr />
+                                <div class="connect row">
+                                    <div class="col-md-4">
+                                        <a href="https://github.com/roblovelett/">
+                                            <img class="img-responsive" src="assets/images/GitHub-Mark-120px-plus.png" alt="GitHub" />
+                                        </a>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <a href="https://linkedin.com/in/roblovelett/">
+                                            <img class="img-responsive" src="assets/images/In-2C-128px-R.png" alt="LinkedIn" />
+                                        </a>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <a href="https://stackoverflow.com/users/6554033">
+                                            <img class="img-responsive" src="assets/images/so-icon.png" alt="Stack Overflow" />
+                                        </a>
+                                    </div>
+                                </div>
+                            </aside>
+                        </div>
+                    </div>  
+                </div>
+            </div>            
+        </main>
+        <footer>
+            <div class="footer">
+                <div class="container">
+                    <div class="row text-center">
+                        &copy; Copyright 2016 Robert Lovelett
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </body>
+</html>
